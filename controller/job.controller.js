17bc908460666d2cf8447793769c5bbf8100d0a3
Chetan kaushik <chetankaushik29@gmail.com>
2024-05-17 22:11:49 +0530

    code update with try cathc in post api

--- controller/job.controller.js
+++ controller/job.controller.js
@@ -2,11 +2,18 @@
 const Job = require("./../model/job.model")
 
 const createJob = async (req, res) => {
+try {
     const newJob = await new Job(req.body)
     const savedData = await newJob.save();
 
     // console.log(savedData);
     res.send(savedData)
+    
+} catch (error) {
+console.log(err,"Something went wrong please try again later")}
+
+
+   
 }
 const getJob = (req, res) => {
     res.json({
